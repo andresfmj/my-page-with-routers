e4fb91b74837bf091e2ba5559f1569c819e3559a
FernanityChamp <5148837+fernanitychamp@users.noreply.github.com>
2018-06-19 11:50:04 -0500

    it added new component page and a link to the Routes

--- src/components/commons/Header.jsx
+++ src/components/commons/Header.jsx
@@ -7,17 +7,20 @@ export default class Header extends React.Component {
         return (
             <header>
                 <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-                    <Link to='/' className='navbar-brand'>Routers!!</Link>
+                    <Link to='/~ferdroid/mi-pagina-react-router/' className='navbar-brand'>Routers!!</Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item active">
-                                <Link to='/' className="nav-link"><span className="sr-only">(current)</span>Home</Link>
+                                <Link to='/~ferdroid/mi-pagina-react-router/' className="nav-link"><span className="sr-only">(current)</span>Home</Link>
                             </li>
                             <li className="nav-item">
-                                <Link to='/contact' className="nav-link">Contact us!</Link>
+                                <Link to='/~ferdroid/mi-pagina-react-router/info' className="nav-link">Information</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to='/~ferdroid/mi-pagina-react-router/contact' className="nav-link">Contact us!</Link>
                             </li>
                         </ul>
                         <form className="form-inline my-2 my-lg-0">
--- src/components/commons/Main.jsx
+++ src/components/commons/Main.jsx
@@ -3,14 +3,16 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home.jsx';
 import ContactPage from '../pages/ContactPage.jsx';
+import InfoPage from '../pages/InfoPage.jsx';
 
 export default class Main extends React.Component {
     render() {
         return (
             <div className='container'>
                 <Switch>
-                    <Route exact path='/' component={Home} />
-                    <Route path='/contact' component={ContactPage} />
+                    <Route exact path='/~ferdroid/mi-pagina-react-router/' component={Home} />
+                    <Route path='/~ferdroid/mi-pagina-react-router/contact' component={ContactPage} />
+                    <Route path='/~ferdroid/mi-pagina-react-router/info' component={InfoPage} />
                 </Switch>
             </div>
         )
